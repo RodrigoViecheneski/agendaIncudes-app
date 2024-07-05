2fbf2c2d764decf32eb79f08d33af58c9cea6e2d
RodrigoViecheneski <rodrigoviecheneski@gmail.com>
2024-07-05 19:50:16 -0300

    layouts

---
+++ resources/views/pagina2.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <h1>Este é o conteúdo da página 2</h1>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,7 @@ Route::get('/', function () {
     return view('include');
 });
 
+Route::get('/layout', [SiteController::class, 'layout']);
+Route::get('/layout2', [SiteController::class, 'layout2']);
+
 Route::get('/components', [SiteController::class,'components']);
